721a943ac2ee0139049795fac4e06fa7ab9729f3
Naveen Gurralu <43296822+NaveenGurralu@users.noreply.github.com>
2022-06-06 16:30:43 +0530

    Add files via upload

---
+++ RG_Creation/Providers.tf
@@ -0,0 +1,23 @@
+terraform {
+
+  required_version = ">=0.12"
+  
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+
+
+
+provider "azurerm" {
+ features {
+
+resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
+
+ }
+}
